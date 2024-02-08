815464f6906ad0afc945096cf6e3bbbef47cc957
kenny akomolafe <akomolafekenny7@gmail.com>
2024-02-08 19:10:32 +0000

    first commit here

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Flower-Classification-Deploy
+
+A user interface for flowers classification of lily, rose and sunflowers using the concept of transfer learning.
---
+++ web.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow as tf 
+import numpy as np
+import keras
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+
+final_model = load_model('C:\\Users\\TeeFaith\\Desktop\\ML PROJECTS\\Flower Classification(deploy)\\Flower Classification Model.h5')
+
+def predict_function(image, final_model):
+    image_array = img_to_array(image)
+    image_dim = np.expand_dims(image_array, axis=0)
+    predict = final_model.predict(image_dim)
+    return predict
+
+image_size = 224
+
+st.title('Flower Classification App (rose, sunflower and lily)')
+file_image = st.file_uploader('Upload your Image', type=['jpeg', 'jpg', 'png', 'gif'])
+if file_image is None:
+    st.write('No file is uploaded here')
+else:
+    image = image.load_img(file_image, target_size=(image_size, image_size))
+    st.image(image, caption='uploaded image', use_column_width=True)
+    predictions = predict_function(image, final_model)
+    string = ''
+    class_names = ['LILY', 'NULL', 'ROSE', 'SUNFLOWERS']
+
+    string = 'This is likely the picture of' + '' + class_names[np.argmax(predictions)]
+
+    st.success(predictions)
+    st.success(string)
+
