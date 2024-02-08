6e4f11465bb9cd1843b5cdfdc8a8e7924baaeaff
kenny akomolafe <akomolafekenny7@gmail.com>
2024-02-08 19:32:43 +0000

    committttss

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Flower-Classification-Deploy
 
 A user interface for flowers classification of lily, rose and sunflowers using the concept of transfer learning.
+updates will be subsequently made
--- web.py
+++ web.py
@@ -5,6 +5,7 @@ import keras
 from keras.preprocessing import image
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
+import numpy as np
 
 final_model = load_model('C:\\Users\\TeeFaith\\Desktop\\ML PROJECTS\\Flower Classification(deploy)\\Flower Classification Model.h5')
 
